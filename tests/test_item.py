db6e39b83171c0d8e4d7535eda5c26c27039e62a
Ivan Zuev <ivan.zuev.1984@mail.ru>
2023-11-13 23:39:59 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def test_class():
+    item = Item(name='some', price=7, quantity=52, )
+    return item
+
+
+def test_calculate_total_price(test_class):
+    assert test_class.calculate_total_price() == 364
+
+
+def test_apply_discount(test_class):
+    assert test_class.apply_discount() == 7
