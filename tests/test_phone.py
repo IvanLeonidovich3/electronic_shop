af54d88a94d9dc0b9d35f70dd27e9bd9688ca81d
Ivan Zuev <ivan.zuev.1984@mail.ru>
2024-01-06 14:06:16 +0300

    add test_add

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.item import Item
 from src.phone import Phone
 
 
@@ -16,3 +17,10 @@ def test_number_of_sim():
     assert phone1.number_of_sim == 1
     with pytest.raises(ValueError):
         phone1.number_of_sim = 0 or -1
+
+
+def test_add():
+    item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
