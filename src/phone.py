7c08a5d2ba50794c223e35582449e072123293d1
Ivan Zuev <ivan.zuev.1984@mail.ru>
2024-01-07 12:59:26 +0300

    добавленна проверка на сложение экземпляров классов Phone and Item"

--- src/phone.py
+++ src/phone.py
@@ -10,6 +10,10 @@ class Phone(Item):
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
+    def __add__(self, other):
+        """проверка на сложение экземпляров классов Phone and Item"""
+        return self.quantity + other.quantity
+
     @property
     def number_of_sim(self):
         return self.__number_of_sim
